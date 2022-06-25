86be020f231a6d50e27ddf63ffb8d72663533f52
Leonardo Aguiar <leonardoaguiar1235@gmail.com>
2022-06-25 15:24:10 -0300

    React

--- App.js
+++ App.js
@@ -1,40 +1,26 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet, Pressable } from 'react-native';
+import React from "react";
+import { TextInput, View } from "react-native";
+import { ButtonText, Container, Login, PressableLogin, Title, InnerText } from "./styles";
 
-
-const Teste = () => {
+const Test = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View style={{ height: 300, width: 300, backgroundColor: "grey" }}>
-        <Text style={{ textAlign: "center", fontSize: 30, fontWeight: "bold", padding: 10 }}>Login</Text>
+    <Container>
+      <Login>
+        <Title>
+          Login
+        </Title>
         <View style={{ marginTop: 20 }}>
-          <View style={{ height: 50, width: 270, backgroundColor: "white", alignSelf: "center" }} />
-          <View style={{ marginTop: 10, height: 50, width: 270, backgroundColor: "white", alignSelf: "center" }} />
+          <InnerText placeholder="username"  />
+          <InnerText placeholder="password"  />
+          <PressableLogin>
+            <ButtonText>
+              Login
+            </ButtonText>
+          </PressableLogin>
         </View>
-        <Pressable style={styles.input} >
-          <Text input style={{ color: 'white', fontSize: 25, }}>Entrar</Text>
-        </Pressable>
-
-
-      </View >
-    </View>
+      </Login>
+    </Container>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  input: {
-    marginTop: 10,
-    backgroundColor: 'black',
-    height: 50,
-    width: 120,
-    alignSelf: 'center',
-    borderRadius: 10,
-    fontWeight: 'bold',
-    alignItems: 'center',
-    justifyContent: 'center',
-    elevation: 5,
-  }
-});
-
-
-export default Teste;
+export default Test;
